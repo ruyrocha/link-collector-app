d17acc43271480a64a758877aa903cdf6ecbd9a3
Ruy R <108208+ruyrocha@users.noreply.github.com>
2023-11-01 14:40:01 -0300

    Add `Home#index` request spec.
    Add Devise related testing stuff.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
+RSpec.describe 'Homes', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  describe 'GET /' do
+    it 'returns HTTP success with a signed in user' do
+      user = create(:user)
+
+      sign_in user
+      get '/'
+
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'returns HTTP redirect without a user session' do
+      get '/'
+
+      expect(response).to have_http_status(:redirect)
+    end
+  end
 end
