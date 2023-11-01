bd4d0f207aa3e0ad2b05701aad1a73ea4f94bc03
Ruy R <108208+ruyrocha@users.noreply.github.com>
2023-11-01 20:24:29 -0300

    Set page title on creation.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,16 @@ class Page < ApplicationRecord
   validates :url, presence: true
 
   has_many :page_links
+
+  before_create :set_title
+
+  private
+
+  def set_title
+    return if title&.present?
+
+    document = PageFetcherService.call(url:)
+
+    self.title = document.title
+  end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Page, type: :model do
   it { is_expected.to validate_presence_of(:url) }
   it { is_expected.to have_many(:page_links) }
+
+  context 'Callbacks' do
+    it 'sets the title when it is empty' do
+      record = build(:page, url: 'https://www.koombea.com/', title: nil)
+
+      record.save
+
+      expect(record.title).not_to be_empty
+      expect(record.title).to eq 'Koombea - Your App Development Partner'
+    end
+  end
 end
