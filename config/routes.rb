cba42065d385061a19f878940a46ac8f5142c800
Ruy R <108208+ruyrocha@users.noreply.github.com>
2023-11-02 12:08:46 -0300

    Fix the root URL.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :page_links
   resources :pages
   devise_for :users
-  root to: 'home#index'
+  root to: 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
